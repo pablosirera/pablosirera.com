c3bf3a4b474cbc1aa86ce8dec9c4b7b4c21d58fb
Pablo Sirera <psirera4@gmail.com>
2020-07-02 10:27:06 +0200

    add mode full static nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-mode
    */
   mode: 'universal',
+  target: 'static',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
