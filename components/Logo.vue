a42b3dfe08a9d3addefcb5c157f2136c11153c17
Pablo <psirera4@gmail.com>
2020-07-31 16:25:14 +0200

    feat: change file name

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <img
     class="w-20 h-20 rounded-full nuxt-logo border-6 border-primary"
-    src="https://res.cloudinary.com/de5xzoviz/image/upload/c_scale,q_58,w_640/v1592684920/IMG_6225_brctd5.jpg"
+    src="https://res.cloudinary.com/de5xzoviz/image/upload/c_scale,q_58,w_640/v1592684920/pablosirera.jpg"
     :alt="$t('general.altLogo')"
     loading="lazy"
   />
