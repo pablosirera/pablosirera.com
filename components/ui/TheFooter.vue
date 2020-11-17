d6738e282525cdffb6b248e7f0bb44e09a7650ad
Pablo Sirera <psirera4@gmail.com>
2020-11-17 13:03:12 +0100

    feat(ui): show current year on footer

--- components/ui/TheFooter.vue
+++ components/ui/TheFooter.vue
@@ -1,12 +1,19 @@
 <template>
   <footer class="text-center p-16">
     Hecho con mucho 💚 por
-    <TheLink url="https://twitter.com/pablosirera">Pablo Sirera</TheLink> © 2020
+    <TheLink url="https://twitter.com/pablosirera">Pablo Sirera</TheLink> ©
+    {{ year }}
   </footer>
 </template>
 
 <script>
 export default {
   name: 'TheFooter',
+  computed: {
+    year() {
+      const date = new Date()
+      return date.getFullYear()
+    },
+  },
 }
 </script>
