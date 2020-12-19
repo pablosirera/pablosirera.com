31b80fc78cd13f891606c8aa674b41cf3e4c9c69
Pablo Sirera <psirera4@gmail.com>
2020-12-19 14:52:04 +0100

    feat(contactMe): do responsive

--- pages/contact-me/index.vue
+++ pages/contact-me/index.vue
@@ -2,7 +2,7 @@
   <section class="text-center flex flex-col justify-center items-center">
     <h2>¿En qué te puedo ayudar?</h2>
     <!-- TODO: create component with form -->
-    <form name="contact" netlify class="text-left w-6/12 mt-5">
+    <form name="contact" netlify class="text-left w-full md:w-8/12 mt-5">
       <p class="flex flex-col mt-2">
         <label class="font-bold mb-1">Nombre</label>
         <input
