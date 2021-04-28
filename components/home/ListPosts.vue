5c03b3a20a0b6eebed458c37a499c9fb700e94bf
Pablo Sirera <psirera4@gmail.com>
2021-04-28 08:32:50 +0200

    fix: decrease images quality

--- components/home/ListPosts.vue
+++ components/home/ListPosts.vue
@@ -10,7 +10,7 @@
       class="link flex justify-start mb-4 rounded p-2 hover:bg-green-main-700-40"
     >
       <img
-        :src="`https://res.cloudinary.com/de5xzoviz/image/upload/q_60/${post.image}`"
+        :src="`https://res.cloudinary.com/de5xzoviz/image/upload/q_55/${post.image}`"
         :alt="post.title"
         class="rounded mr-3 w-auto"
         loading="lazy"
