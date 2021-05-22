a98063ee4952845ba0034250fda581251ee6f94d
Pablo Sirera <psirera4@gmail.com>
2021-05-22 19:04:55 +0200

    fix: update year footer

--- components/ui/TheFooter.vue
+++ components/ui/TheFooter.vue
@@ -1,19 +1,12 @@
 <template>
   <footer class="container text-center py-16 px-3">
     Hecho con mucho 💚 por
-    <TheLink url="https://twitter.com/pablosirera">Pablo Sirera</TheLink> ©
-    {{ year }}
+    <TheLink url="https://twitter.com/pablosirera">Pablo Sirera</TheLink> © 2020
   </footer>
 </template>
 
 <script>
 export default {
   name: 'TheFooter',
-  computed: {
-    year() {
-      const date = new Date()
-      return date.getFullYear()
-    },
-  },
 }
 </script>
