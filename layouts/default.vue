b279c15409a1a9de613d4fecb8f17eca1790f546
Emiliano <emiherbes@hotmail.com>
2021-10-14 14:39:41 -0300

    altura minina al main

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="min-h-screen">
     <BaseHeader />
-    <main class="pt-4 container px-3">
+    <main class="pt-4 container px-3 min-h-main">
       <nuxt />
     </main>
     <BaseFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.min-h-main {
+  min-height: 54vh;
+}
+</style>
